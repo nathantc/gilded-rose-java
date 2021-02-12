a2141c728bb7b64639beb8ef606cb9db9dcf5216
Nathan Carver <nathan@codecarver.io>
2021-02-11 21:20:56 -0500

    Added tests for quality degrade each day.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void itemShouldHaveName() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void itemDegradesByOneEachDay(){
+        Item[] items = new Item[] { new Item("foo", 6, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4,app.items[0].quality);
+    }
+
+    @Test
+    void qualityDegradesTwiceAfterSellByDate() {
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8,app.items[0].quality);
+    }
+
+    @Test
+    void qualityCanNeverBeNegative() {
+        Item[] items = new Item[] { new Item("foo", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].quality);
+    }
 }
