2b14d134bf9c6807c5c1e7c5dbc9dd8d60a2ff6c
Carl Scozzari <carlscozzari@Carls-MacBook-Pro.local>
2021-02-16 23:29:23 -0500

    qualtiy wont decrease for Sulfuras test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,51 +9,58 @@ class GildedRoseTest {
 
     @Test
     void itemShouldHaveName() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
     @Test
-    void itemDegradesByOneEachDay(){
-        Item[] items = new Item[] { new Item("foo", 6, 5) };
+    void itemDegradesByOneEachDay() {
+        Item[] items = new Item[]{new Item("foo", 6, 5)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(4,app.items[0].quality);
+        assertEquals(4, app.items[0].quality);
     }
 
     @Test
     void qualityDegradesTwiceAfterSellByDate() {
-        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        Item[] items = new Item[]{new Item("foo", 0, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(8,app.items[0].quality);
+        assertEquals(8, app.items[0].quality);
     }
 
     @Test
     void qualityCanNeverBeNegative() {
-        Item[] items = new Item[] { new Item("foo", 5, 0) };
+        Item[] items = new Item[]{new Item("foo", 5, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(0,app.items[0].quality);
+        assertEquals(0, app.items[0].quality);
     }
 
     @Test
     void qualityIncreasesForAgedBrie() {
-        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 5)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(6,app.items[0].quality);
+        assertEquals(6, app.items[0].quality);
     }
 
     @Test
     void qualityOfItemRemainsSameWhenOverFifty() {
-        Item[] items = new Item[] { new Item("Aged Brie", 3,51)};
+        Item[] items = new Item[]{new Item("Aged Brie", 3, 51)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(2,app.items[0].sellIn);
+        assertEquals(2, app.items[0].sellIn);
     }
 
+    @Test
+    void qualityNeverDecreasesWhenSulfuras() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 8, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(30, app.items[0].quality);
 
+    }
 }
